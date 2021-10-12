0661aa9661e8cb2f5f3e02eb93496783b11945be
carker-7 <carker-7@student.ltu.se>
2021-10-12 14:37:53 +0200

    switch to vm

--- node.go
+++ node.go
@@ -24,17 +24,17 @@ func main() {
     conn,_ := net.Dial("ip:icmp","google.com")
     fmt.Println(conn.LocalAddr())
     
-    if (conn.LocalAddr().String() == "172.19.0.2"){
-        me := d7024e.NewContact(d7024e.NewKademliaID("0000000000000000000000000000000000000000"), "172.19.0.2:8080") 
+    if (conn.LocalAddr().String() == "172.21.0.2"){
+        me := d7024e.NewContact(d7024e.NewKademliaID("0000000000000000000000000000000000000000"), "172.21.0.2:8080") 
         rt := d7024e.NewRoutingTable(me)
         network := d7024e.NewNetwork(&me, rt)
         kademlia := d7024e.NewKademlia(*rt, network)
         go startAPI(kademlia)
         go startCLI(kademlia)
         network.Listen("8080")
-    } else if (conn.LocalAddr().String() == "172.19.0.3") {
-        known := d7024e.NewContact(d7024e.NewKademliaID("0000000000000000000000000000000000000000"), "172.19.0.2:8080") 
-        me := d7024e.NewContact(d7024e.NewKademliaID("0000000000000000000000000000000000000001"), "172.19.0.3:8080")
+    } else if (conn.LocalAddr().String() == "172.21.0.3") {
+        known := d7024e.NewContact(d7024e.NewKademliaID("0000000000000000000000000000000000000000"), "172.21.0.2:8080") 
+        me := d7024e.NewContact(d7024e.NewKademliaID("0000000000000000000000000000000000000001"), "172.21.0.3:8080")
         rt := d7024e.NewRoutingTable(me)
         rt.AddContact(known)
         network := d7024e.NewNetwork(&me, rt)
@@ -44,7 +44,7 @@ func main() {
         go startCLI(kademlia)
         network.Listen("8080")
     } else {
-        known := d7024e.NewContact(d7024e.NewKademliaID("0000000000000000000000000000000000000000"), "172.19.0.2:8080") 
+        known := d7024e.NewContact(d7024e.NewKademliaID("0000000000000000000000000000000000000000"), "172.21.0.2:8080") 
         me := d7024e.NewContact(d7024e.NewRandomKademliaID(), d7024e.GetLocalIP()+":8080")
         rt := d7024e.NewRoutingTable(me)
         rt.AddContact(known)
